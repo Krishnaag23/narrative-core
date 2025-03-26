2b169f22156a5a0e9e04a20b0691355c98560c89
Krishnaag23 <krishnaag23@iitk.ac.in>
2025-03-27 00:37:30 +0530

    chore : add initial ideation comments

---
+++ src/input_processing/input.py
@@ -0,0 +1,29 @@
+''' I want to have a series of questions / options that the user can answer/select and based on that 
+I would have a basic idea of the kind of story they wish to generate, do I just ask question and get 
+the concept from there or employ some nlp technique to extract the basic idea, can this even be done 
+with nlp?'''
+'''what sort of questions? what would be the input and output of this class? '''
+'''I want the input to be all the text content in a dictionary format and the process it and return
+concept in another dictionary format'''
+
+
+'''List of elements of a story :- 
+    -character
+    (would be defined in the character_system in a more verbose manner)
+        protagonist, antagonist, supporting characters ,relationships 
+    -plot :
+        rising action, climax, falling action, and resolution. 
+    -setting :
+        time and place where the story unfolds, including the physical environment and cultural context. 
+    -Theme:
+        underlying message or idea explored in the story, often dealing with universal human experiences or concepts. 
+    -Point of View:
+        perspective from which the story is told, which can be first-person, third-person limited, or third-person omniscient. 
+    -Conflict:
+        central problem or struggle that drives the plot forward, creating tension and drama. 
+    -Tone:
+        the author's attitude or feeling towards the subject matter, which can be conveyed through word choice and style. 
+'''
+
+
+    
